90cce9a05c069ef1708a9840b41a7969bf5d8eea
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-04-27 15:56:09 -0300

    Componente Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,17 @@
+import type {StoryObj, Meta} from '@storybook/react'
+
+import { Text, TextProps } from '@nikolas-ui/react'
+
+export default {
+    title: 'Typography/Text', 
+    component: Text,  
+    
+    args: {
+       children: 'Exemplo de texto',
+    }
+
+}as Meta<TextProps>
+
+
+export const Primary: StoryObj<TextProps> = {}
+

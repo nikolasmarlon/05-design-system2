07d42c9876a202bc58114fc5cf5e6993f7994b73
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-04-30 00:15:27 -0300

    Componente Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,25 @@
+import type {StoryObj, Meta} from '@storybook/react'
+
+import { Avatar, AvatarProps } from '@nikolas-ui/react'
+
+export default {
+    title: 'Data display/Avatar', 
+    component: Avatar,  
+    
+    args: {
+       src: 'https://github.com/nikolasmarlon.png',
+       alt: 'Nikolas Marlon',
+    }
+
+}as Meta<AvatarProps>
+
+
+export const Primary: StoryObj<AvatarProps> = {}
+export const WithFallback: StoryObj<AvatarProps> = {
+    args: {
+        src: undefined,
+    }
+}
+
+
+

7a613dd1f2160c022bc3a34dd992901c5e31c29e
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-05-08 19:43:05 -0300

    Update main.js

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,6 +15,13 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
-  }
+  },
   
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/'
+    }
+
+    return config
+  }
 }
